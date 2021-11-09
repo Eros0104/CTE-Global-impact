542f5c83c1f53699db314119b4c611b8bedae60b
Eros0104 <erosmendonca@outlook.com>
2021-11-09 15:05:10 -0300

    feat: Redução do código de cálculos de porcentagens

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ import json
 import pandas as pd
 import webbrowser
 
-km = 27
 routes = json.load(open("data/routes.json"))
-
 
 render_header()
 for route in routes:
@@ -23,30 +21,31 @@ for route in routes:
     anual_passengers = route.get("anualPassengers")
     # 1 Cálculo da tarifa ao passageiro por trecho
     route["user_tax"] = calculate_user_tax_by_kilometer(distance)
+
     # 2) Cálculo do custo operacional anual por trecho
     route["anual_cost"] = calculate_anual_cost_by_kilometer(distance)
+
     # 3) Cálculo  da  projeção  anual  de  receitas
-    route["anual_projection_30"] = calculate_anual_projection_by_percentage(
-        30, distance, anual_passengers
-    )
-    route["anual_projection_40"] = calculate_anual_projection_by_percentage(
-        40, distance, anual_passengers
-    )
-    route["anual_projection_50"] = calculate_anual_projection_by_percentage(
-        50, distance, anual_passengers
-    )
-    route["anual_projection_60"] = calculate_anual_projection_by_percentage(
-        60, distance, anual_passengers
-    )
+    percentages = [30, 40, 50, 60]
+
+    for percentage in percentages:
+        route[
+            "anual_projection_%i" % percentage
+        ] = calculate_anual_projection_by_percentage(
+            percentage, distance, anual_passengers
+        )
+
     # 4) calcular  a  quantidade  anual mínima de passageiros necessária para garantir um lucro de 10%
     minimal_passengers = calculate_minimal_passengers_to_profit_10_percent(
         route["user_tax"], route["anual_cost"], anual_passengers
     )
     route["minimal_passengers_to_profit"] = minimal_passengers
+
     # 5) verificar se trecho é viável
-    route["is_viable"] = route.get("minimal_passengers_to_profit") <= route.get(
+    is_viable = route.get("minimal_passengers_to_profit") <= route.get(
         "anualPassengers"
     )
+    route["is_viable"] = is_viable
     render_row(route)
 render_line()
 
